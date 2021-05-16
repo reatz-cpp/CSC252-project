0f2d230c2ccb0a4515612ba38113cced23a65ab0
daniel.reatz <faceace69@gmail.com>
2021-05-16 11:31:23 -0500

    Update with main

--- robot.cpp
+++ robot.cpp
@@ -1,9 +1,43 @@
 #include "robot.h"
 
-Robot::Robot(int lx, int ly, bool cb, char ld)
+void printBoard(char a[][10], int size)
 {
-	cargoBed = cb;
-	load = ld;
+	//nested loop to print a 2d array
+	for (int i = 0; i < 10; i++)
+		for (int j = 0; j < 10; j++)
+			cout << board[i][j] << endl;
+
+}
+
+void clear(char a[][10], int size)
+{
+	Robot* r;
+	//use a nested loop to traverse the board and at each cell with a load
+	for (int i = 0; i < size; i++)
+		for (int j = 0; j < size; j++)
+		{
+			board[i][j];
+			r->pickUp(i, j);
+			//dynamically create a Robot object and place at that cell
+				//use the robot to pick up the load 
+			//Now that the cell is clear, return the memory back to system
+		}
+			
+
+}
+
+Robot::Robot()
+{
+	xLocation = 0;
+	yLocation = 0;
+	cargoBed = false;
+	load = '.';
+}
+
+Robot::Robot(int lx, int ly, bool cargoBed, char load)
+{
+	this->cargoBed = cargoBed;
+	this->load = load;
 	xLocation = lx;
 	yLocation = ly;
 	board[lx][ly] = 0;
@@ -28,37 +62,90 @@ char Robot::getLoad()
 }
 void Robot::setLx(int lx)
 {
-	xLocation = lx;
+	if(lx >= 0 && lx <10)
+		xLocation = lx;
 }
 void Robot::setLy(int ly)
 {
-	yLocation = ly;
+	if(ly >= 0 && ly < 10)
+		yLocation = ly;
 }
-void Robot::setCb(bool cb)
+void Robot::setCb(bool cargoBed)
 {
-	cargoBed = cb;
+	this->cargoBed = cargoBed;
 }
-void Robot::setLoad(char ld)
+void Robot::setLoad(char load)
 {
-	load = ld;
+	this->load = load;
 }
 bool Robot::moveTo(int lx, int ly)
 {
-	for (int i = 0; i < 10; i++)
-		for (int j = 0; j < 10; j++)
-		{
-			board[i][j];
-			if (board[lx][ly] != board[i][j])
-				return false;
+	assert(lx >= 0 && lx < 10);
+	assert(ly >= 0 && ly < 10);
 
-		}
+	int diffInX = xLocation - lx;
+	int diffInY = yLocation - ly;
+	int xSteps = abs(diffInX);
+	int ySteps = abs(diffInY);
+	if (diffInX > 0)
+	{
+		for (int i = 0; i < xSteps; i++)
+			xLocation--;
+	}
+		
+	else
+	{
+		for (int i = 0; i < xSteps: i++)
+			xLocation++;
+	}
+		
+
 	return true;
 
 }
 
 bool Robot::pickUp(int lx, int ly)
 {
-	load;
-	board[lx][ly];
-	cargoBed;
+	assert(lx >= 0 && lx < 10);
+	assert(ly >= 0 && ly < 10);
+	if (board[lx][ly] != '.')//if board is not empty
+	{
+		if (cargoBed = false)
+		{
+			//move robot to the location and place load in cargobed
+			//make sure to reset the board to empty and cargobed to true
+			return true;
+		}
+		else
+		{
+			cout << "Robot has a load already\n";
+			return false;
+		}
+		
+	}
+	cout << "No load at board location specified\n";
+	return false;
+}
+
+bool Robot::dropOff(int lx, int ly)
+{
+	assert(lx >= 0 && lx < 10);
+	assert(ly >= 0 && ly < 10);
+	if (board[lx][ly] = '.')//board is empty)
+	{
+		if (load != 0) //Robot has a load)
+		{
+			//move robot to the location and place load in cargobed
+			//make sure to reset the board to full and cargobed to false
+			return true;
+		}
+		else
+		{
+			cout << "Robot has no load to drop off\n";
+			return false;
+		}
+
+	}
+	cout << "A load is already at board location specified\n";
+	return false;
 }
--- robot.h
+++ robot.h
@@ -1,28 +1,33 @@
 #ifndef ROBOT_H
 #define ROBOT_H
 #include <iomanip>
+#include <iostream>
+#include <vector>
+#include <ctime>
 #include <string>
+#include <assert.h>
 
 using namespace std;
 
-int board[10][10];
+char board[10][10];
 
 class Robot
 {
 
 public:
-	Robot(int lx, int ly, bool cb, char ld);
+	Robot();
+	Robot(int lx, int ly, bool cargoBed, char load);
 	int getLx();
 	int getLy();
 	bool getCb();
 	char getLoad();
 	void setLx(int lx);
 	void setLy(int ly);
-	void setCb(bool cb);
-	void setLoad(char ld);
+	void setCb(bool cargoBed);
+	void setLoad(char load);
 	bool moveTo(int lx, int ly);
 	bool pickUp(int lx, int ly);
-
+	bool dropOff(int lx, int ly);
 
 private:
 	int xLocation;
